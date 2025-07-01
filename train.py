6f3e2fb2369c33d518f7c23357e15e6e8f994269
Bilal143260 <smbilal642@gmail.com>
2025-07-01 08:55:30 +0000

    fixed dataset initialization

--- train.py
+++ train.py
@@ -553,20 +553,19 @@ def main(args):
 
     # Dataset and DataLoaders creation:
     train_dataset = KontextDataset(
-        instance_data_root=args.instance_data_dir,
-        instance_prompt=args.instance_prompt,
-        class_prompt=args.class_prompt,
-        class_data_root=args.class_data_dir if args.with_prior_preservation else None,
-        class_num=args.num_class_images,
-        buckets=buckets,
-        repeats=args.repeats,
-        center_crop=args.center_crop,
+        dataset_name=args.dataset_name,
+        source_column_name=args.source_column,
+        target_column_name=args.target_column,
+        caption_column_name=args.caption_column,
     )
+
     train_dataloader = torch.utils.data.DataLoader(
         train_dataset,
-        collate_fn=lambda examples: collate_fn(examples, args.with_prior_preservation),
+        collate_fn=collate_fn,
         num_workers=args.dataloader_num_workers,
     )
+
+    #TODO: will add validation dataset and dataloader later
 
     if not args.train_text_encoder:
         tokenizers = [tokenizer_one, tokenizer_two]
